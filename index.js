b9c0063909096a1fe9c5ec8e2644581ac7b91480
Ayush Soni <soni.ayush.2212@gmail.com>
2023-06-27 22:43:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/indexRoutes");
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+app.use("/", indexRouter);
+
+app.listen(4000, () => {
+  console.log("Listening on PORT 4000");
+});
